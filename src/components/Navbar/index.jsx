7b114a0ef8105eccdde682978fc0339f4df19c83
PierreGatschine <pierre.gatschine@gmail.com>
2022-03-21 17:19:06 +0100

    Add SignOut, SignIn LogOut

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,48 @@
+import React, {useContext} from 'react'
+import {UserContext} from '../../context/userContext';
+import { Link, useNavigate } from 'react-router-dom'
+import { signOut } from 'firebase/auth';
+import { auth } from '../../firebase-config';
+
+
+
+export default function Navbar() {
+    const {toggleModals} = useContext(UserContext)
+
+    const navigate = useNavigate()
+
+    const logOut = async () => {
+        try {
+            await signOut(auth)
+            navigate("/")
+        } catch (error) {
+            alert("For some reasons we can't deconnect, please check your internet connection and retry.")
+        }
+    }
+
+
+  return (
+    <nav className="navbar navbar-light bg-light px-4">
+        <Link to="/" className="navbar-brand">
+            AuthJS
+        </Link>
+        <div>
+            <button 
+            onClick={() => toggleModals('signUp')}
+            className="btn btn-primary">
+                Sign Up
+            </button>
+            <button
+            onClick={() => toggleModals('signIn')} 
+            className="btn btn-primary ms-2">
+                Sign In
+            </button>
+            <button 
+            onClick={logOut}
+            className="btn btn-danger ms-2">
+                Log Out
+            </button>
+        </div>
+    </nav>
+  )
+}
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,17 @@
+import React, {useContext} from 'react'
+import { UserContext } from '../../context/userContext'
+
+
+export default function Home() {
+
+  const {currentUser} = useContext(UserContext)
+
+  return (
+    <div className="container p-5">
+      <h1 className="display-3 text-light">
+        {currentUser ? "Welcome buddy !" : "Please sign up or sign in"}
+      </h1>
+
+    </div>
+  )
+}
